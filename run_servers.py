edc3675e2a4b5834db908b633d0d9af06a4719d5
Ubuntu <ubuntu@ip-172-31-38-247.eu-central-1.compute.internal>
2025-06-16 23:24:02 +0300

    work

--- run_servers.py
+++ run_servers.py
@@ -1,5 +1,5 @@
 import subprocess
 
 if __name__ == "__main__":
-   # subprocess.Popen(["uvicorn", "server_app:app", "--reload", "--host", "0.0.0.0", "--port", "5001"])
+    subprocess.Popen(["uvicorn", "server_app:app", "--reload", "--host", "0.0.0.0", "--port", "5001"])
     subprocess.Popen(["uvicorn", "server_reminder:app", "--reload", "--host", "0.0.0.0", "--port", "5000"])
